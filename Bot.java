851f88996c0eabfec958853312c4ebf1c3853924
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2024-11-11 15:54:10 -0800

    added comments to Bot.java

--- Bot.java
+++ Bot.java
@@ -1,9 +1,8 @@
 public class Bot extends Player{
     public Bot(){
+        //Bot is a child class of player and has all the attributes the player has
+        // Its just that its given name is Bot.
+        //@param: takes String called inputName
         super("Bot");
     }
-
-
-
-
 }
