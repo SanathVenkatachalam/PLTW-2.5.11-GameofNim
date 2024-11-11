c25f1b4ce2bac06550abea1871e4da762c202218
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2024-11-11 15:52:58 -0800

    fix

--- Player.java
+++ Player.java
@@ -1,54 +1,45 @@
 /*
  * Activity 2.5.2
- * 
+ *
  * A Player class the PhraseSolverGame
  */
+
 import java.util.Scanner;
 
-public class Player
-{
-  /* your code here - attributes */
+public class Player {
+    /* your code here - attributes */
+    String name;
 
-  /* your code here - constructor(s) */ 
-  public Player(){
-    System.out.println("Enter player name: ");
-    Scanner scanner = new Scanner(System.in);
-    String newName = scanner.nextLine();
 
-    String name = newName;
-    System.out.println("Hello and welcome to the game " + name);
-    int points = 0;
+    /* your code here - constructor(s) */
+    public Player() {
+        System.out.println("Enter player name: ");
+        Scanner scanner = new Scanner(System.in);
+        String newName = scanner.nextLine();
 
-  }
-  public Player(String inputName){
-    String name = inputName;
-    System.out.println("Hello and welcome to the game " + name);
-    points = 0;
-  }
+        name = newName;
+        System.out.println("Hello and welcome to the game " + name);
 
-  /* your code here - accessor(s) */ 
-  //returns the player name
-  public String getName(){
-    return name;
-  }
-  //returns the amount of points a player has
-  public int getPoints(){
-    return points;
-  }
-  
+    }
 
-  /* your code here - mutator(s) */ 
+    public Player(String inputName) {
+        name = inputName;
+        System.out.println("Hello and welcome to the game " + name);
+    }
+
+    /* your code here - accessor(s) */
+    //returns the player name
+    public String getName() {
+        return name;
+    }
+
+
+    /* your code here - mutator(s) */
   /*sets the player's name
-  @param inputName a String containing characters that represents someone's name 
+  @param inputName a String containing characters that represents someone's name
   */
-  public void setName(String inputName){
-    //changes the name variable to the input
-    name = inputName
-  }
-  /*adds the points for the score
-  @param value a numerical value that is used to add points
-  */
-  public void addToPoints(int value){
-    points+=value;
-  }
+    public void setName(String inputName) {
+        //changes the name variable to the input
+        name = inputName;
+    }
 }
