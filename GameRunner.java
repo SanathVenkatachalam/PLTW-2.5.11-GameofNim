8ef088cb794cc9645aee776531b97f89bd696915
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2024-11-11 15:55:25 -0800

    commenting GameRunner.java

--- GameRunner.java
+++ GameRunner.java
@@ -1,13 +1,11 @@
-public class GameRunner
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Welcome to the Game of Nim!");
+public class GameRunner {
+    public static void main(String[] args) {
+        //Welcome message
+        System.out.println("Welcome to the Game of Nim!");
+        //create instance of Game called nim(Get it? Because it's called Game of Nim.)
+        Game nim = new Game();
+        //called the play() function to start the game.
+        nim.play();
 
-    
-    
-    Game nim = new Game(); 
-    nim.play();
-    
-  }
+    }
 }
